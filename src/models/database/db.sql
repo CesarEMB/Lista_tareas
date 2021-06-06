d3ac6c684aed362d3bbc42c55b8d67f561e2fb1f
unknown <cesarmb26@gmail.com>
2021-06-06 18:12:02 -0430

    modificacion

--- src/models/database/db.sql
+++ src/models/database/db.sql
@@ -4,8 +4,5 @@ CREATE DATABASE lista_tareas;
 CREATE TABLE tareas (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Tarea VARCHAR(100) NOT NULL,
-    Fecha_tarea VARCHAR(100),
+    Fecha_tarea VARCHAR(100)
 );
-
-
-
